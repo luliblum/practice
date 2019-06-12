3686882646cc49f13a91be78a0afcee0d5a05417
luliblum <luli.blum@gmail.com>
2019-06-12 03:06:51 -0400

    grep.py modify

--- ex5/q1/b/grep.py
+++ ex5/q1/b/grep.py
@@ -19,11 +19,13 @@ for arg in sys.argv:
 print ("----------------------------------------------")
 if __name__ == '__main__':
     if len(argv) != 3:
+            print ("usage: <path_file> <word>")
         
             raise Exception ("Wrong number of arguments. Please provide two argumets: file path and word!")
 
     file_name, word = argv[1], argv[2]
     if not path.isfile(file_name):
+            print ("usage: <path_file> <word>")
             raise Exception ("The provided path doesn't point to an existing path!")
     print ("")
     print ("")
