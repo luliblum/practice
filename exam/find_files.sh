cd06327e64a2ef33552b107cc5f7d9f99967abbd
luliblum <luli.blum@gmail.com>
2019-06-13 14:26:12 -0400

    find_files.sh is added

--- exam/find_files.sh
+++ exam/find_files.sh
@@ -5,6 +5,33 @@
 #  echo "$p"
 #done <text.txt
 
+#check args
 
-for word in $(cat text.txt); do echo $word; done
-echo $word
+function usage() {
+        if [[ -z "$1" ]] || [[ -z "$2" ]]; then
+                echo "You must enter the directory_path and the word to search"
+		echo "One of the parameters is missing"
+                echo "usage: [word to search] [directory_path]"
+                exit 1
+        fi
+} #usage
+
+usage $1, $2
+
+#extract passed args to readable vars
+word=$1
+DIR=$2
+
+if [[ -d $DIR ]]
+then
+  echo "The string '$word' find in this files:"
+  echo "-----------------------------------"
+  egrep -ril  $word $DIR
+else
+ echo "The path '$DIR' directory does not exists"
+fi
+
+#egrep -ril $dir_path  $word
+
+
+
